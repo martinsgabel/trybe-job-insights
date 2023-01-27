dad28fd348e06676dc44c9631dc72791545d4b34
martins <gabrielamspinola@gmail.com>
2023-01-27 13:53:36 -0300

    fix: erros de código

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,5 +2,5 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    res = read_brazilian_file("mocks/brazilian_jobs.csv")
-    assert res[0].keys() == (['title', 'salary', 'type'])
+    res = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert list(res[0].keys()) == (['title', 'salary', 'type'])
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,8 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    res = count_ocurrences("mocks/jobs.csv", "End")
-    assert res == 3
+    res = count_ocurrences("data/jobs.csv", "Python")
+    assert res == 1639
+
+    res = count_ocurrences("data/jobs.csv", "javascript")
+    assert res == 122
